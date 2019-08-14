7c7440ac8fb44f36a27fb75c24e95004d1ea2671
Joshua Hall <alamoscout117@gmail.com>
2019-08-13 20:24:13 -0400

    Creating a skeleton website

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get('/cool', function(req, res, next) {
+  res.send('you\'re cool');
+});
 
 module.exports = router;
